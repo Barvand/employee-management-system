21b444091095562ec200d6688e628d14ffbbe743
B <128028346+Barvand@users.noreply.github.com>
2025-10-01 19:51:06 +0200

    changed URL for API

--- src/axios.ts
+++ src/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "database-migration-i4zk68kb1-barvands-projects.vercel.app/api",
   withCredentials: true,
 });
